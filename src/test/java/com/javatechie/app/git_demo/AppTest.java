a163d1a8d25cd515b36bc4aebd40b79458613338
chandan kumar <rockchandan.94@gmail.com>
2022-04-01 02:01:16 +0530

    added git-demo project to local repo

---
+++ src/test/java/com/javatechie/app/git_demo/AppTest.java
@@ -0,0 +1,20 @@
+package com.javatechie.app.git_demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
